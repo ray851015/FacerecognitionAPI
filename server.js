40d28fe422d889b81c012936f0b6360e0f9e95db
ray851015 <ray851015@gmail.com>
2021-09-02 19:11:25 +0800

    database update

--- server.js
+++ server.js
@@ -12,10 +12,9 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'postgres',
-      database : 'smartbrain'
+      connectionString: process.env.DATABASE_URL,
+      ssl: true,
+      
     }
   });
 
